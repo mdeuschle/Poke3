d2119cb40338ea3fb54755db92c6cd822bdca16a
Matt Deuschle <mdeuschle@gmail.com>
2016-12-23 16:22:06 -0800

    set up ui for root vc

---
+++ Poke3/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RootViewController.swift
+//  Poke3
+//
+//  Created by Matt Deuschle on 12/23/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
