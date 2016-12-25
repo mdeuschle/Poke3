8d2c2242ca14598478b09de09a4c82e3a7bf1c63
Matt Deuschle <mdeuschle@gmail.com>
2016-12-24 17:11:16 -0800

    added outlets and vars

--- Poke3/PokeDetailViewController.swift
+++ Poke3/PokeDetailViewController.swift
@@ -12,9 +12,31 @@ class PokeDetailViewController: UIViewController {
 
     var poke: Pokemon!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var mainImage: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var defenseLabel: UILabel!
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var pokeIDLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var baseID: UILabel!
+    @IBOutlet var currentEvoImage: UIImageView!
+    @IBOutlet var nextEvoImage: UIImageView!
+    @IBOutlet var evoLabel: UILabel!
+
+
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+
+    @IBAction func backPuttonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
 }
